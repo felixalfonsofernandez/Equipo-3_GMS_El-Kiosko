36a6687550560ea9aceb41c2f5da8508fe33df6b
felixalfonsofernandez <felix.fernandez2@unmsm.edu.pe>
2023-01-30 13:45:28 -0500

    Cambios del codigo

--- src/Modelo/ProveedorDao.java
+++ src/Modelo/ProveedorDao.java
@@ -14,29 +14,45 @@ public class ProveedorDao {
     PreparedStatement ps;
     ResultSet rs;
     public boolean RegistrarProveedor(Proveedor pr){
-        String sql = "INSERT INTO proveedor(ruc, nombre, telefono, direccion) VALUES (?,?,?,?)";
-        try {
-           con = cn.getConnection();
-           ps = con.prepareStatement(sql);
-           ps.setString(1, pr.getRuc());
-           ps.setString(2, pr.getNombre());
-           ps.setString(3, pr.getTelefono());
-           ps.setString(4, pr.getDireccion());
-           ps.execute();
-           return true;
-        } catch (SQLException e) {
-            System.out.println(e.toString());
-            return false;
-        }finally{
-            try {
-                con.close();
-            } catch (SQLException e) {
-                System.out.println(e.toString());
+        // Se establecera los limites para validar a un proveedor
+        if(pr.getRuc().length()>0 && pr.getNombre().length()>0 && pr.getTelefono().length()>0 && pr.getDireccion().length()>0 ){
+            if(pr.getTelefono().length()==9){
+                if(pr.getRuc().length()==11){
+                    String sql = "INSERT INTO proveedor(ruc, nombre, telefono, direccion) VALUES (?,?,?,?)";
+                    try {
+                       con = cn.getConnection();
+                       ps = con.prepareStatement(sql);
+                       ps.setString(1, pr.getRuc());
+                       ps.setString(2, pr.getNombre());
+                       ps.setString(3, pr.getTelefono());
+                       ps.setString(4, pr.getDireccion());
+                       ps.execute();
+                       return true;
+                    } catch (SQLException e) {
+                        System.out.println(e.toString());
+                        return false;
+                    }finally{
+                        try {
+                            con.close();
+                        } catch (SQLException e) {
+                            System.out.println(e.toString());
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
             }
+        } else {
+            return false;
         }
+        
+        // se procedio a realizar la validación de los datos segun cada proveedor
     }
     
     public List ListarProveedor(){
+        //Listar proveedor se realizo un cambio para mostrar solo las que tengan sus campos llenos
         List<Proveedor> Listapr = new ArrayList();
         String sql = "SELECT * FROM proveedor";
         try {
@@ -80,26 +96,39 @@ public class ProveedorDao {
     }
     
     public boolean ModificarProveedor(Proveedor pr){
-        String sql = "UPDATE proveedor SET ruc=?, nombre=?, telefono=?, direccion=? WHERE id=?";
-        try {
-            con = cn.getConnection();
-            ps = con.prepareStatement(sql);
-            ps.setString(1, pr.getRuc());
-            ps.setString(2, pr.getNombre());
-            ps.setString(3, pr.getTelefono());
-            ps.setString(4, pr.getDireccion());
-            ps.setInt(5, pr.getId());
-            ps.execute();
-            return true;
-        } catch (SQLException e) {
-            System.out.println(e.toString());
-            return false;
-        }finally{
-            try {
-                con.close();
-            } catch (SQLException e) {
-                System.out.println(e.toString());
+        //Se establecio un limite para modificar proveedor
+        if(pr.getRuc().length()>0 && pr.getNombre().length()>0 && pr.getTelefono().length()>0 && pr.getDireccion().length()>0 ){
+            if(pr.getTelefono().length()==9){
+                if(pr.getRuc().length()==11){
+                    String sql = "UPDATE proveedor SET ruc=?, nombre=?, telefono=?, direccion=? WHERE id=?";
+                    try {
+                       con = cn.getConnection();
+                       ps = con.prepareStatement(sql);
+                       ps.setString(1, pr.getRuc());
+                       ps.setString(2, pr.getNombre());
+                       ps.setString(3, pr.getTelefono());
+                       ps.setString(4, pr.getDireccion());
+                       ps.setInt(5, pr.getId());
+                       ps.execute();
+                       return true;
+                    } catch (SQLException e) {
+                        System.out.println(e.toString());
+                        return false;
+                    }finally{
+                        try {
+                            con.close();
+                        } catch (SQLException e) {
+                            System.out.println(e.toString());
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
             }
+        } else {
+            return false;
         }
     }
 }
